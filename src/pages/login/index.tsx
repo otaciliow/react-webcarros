72664c885fb433570428c4e429db5da6ec47ba55
otaciliow <otacilio.dml@hotmail.com>
2025-02-11 10:09:13 -0300

    feat: add button's style

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -30,7 +30,7 @@ export function Login() {
                 <Link to="/" className="mb-6 max-w-sm w-full">
                     <img src={logoImg} alt="logo do site"  className="w-full"/>
                 </Link>
-                <form action="" className="bg-white max-w-xl w-full rounded-lg" onSubmit={handleSubmit(onSubmit)}>
+                <form action="" className="bg-white max-w-xl w-full rounded-lg p-4" onSubmit={handleSubmit(onSubmit)}>
                     
                     <div className="mb-3">
                         <Input type="email" placeholder="Digite seu e-mail..." name="email" error={errors.email?.message} register={register}/>
@@ -40,7 +40,7 @@ export function Login() {
                         <Input type="password" placeholder="Digite sua senha..." name="password" error={errors.password?.message} register={register}/>
                     </div>
 
-                    <button>Acessar</button>
+                    <button className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium">Acessar</button>
                 </form>
             </div>
 
