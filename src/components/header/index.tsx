a17340262f4e59a2c92bc05decb5ab9c9c2fdada
otaciliow <otacilio.dml@hotmail.com>
2025-02-10 23:26:25 -0300

    feat: added header and logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,33 @@
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/logo.svg';
+import { FiUser, FiLogIn } from 'react-icons/fi';
+
 export function Header() {
+    const signed = false;
+    const loadingAuth = false;
+
     return (
-        <div>
-            <header>Header</header>
-        </div>
+        <header className="w-full flex items-center justify-center h-16 bg-white drop-shadow">
+            <div className="flex w-full max-w-7xl items-center justify-between px-4 mx-auto">
+                <Link to='/'>
+                    <img src={logoImg} alt="Logo do site" />
+                </Link>
+
+                <div className="border-2 rounded-full p-1 border-gray-900">
+                    { !loadingAuth && signed && (
+                        <Link to='/dashboard'>
+                            <FiUser size={22} color="#000" />
+                        </Link>
+                    )}
+
+                    { !loadingAuth && !signed && (
+                        <Link to='/login'>
+                            <FiLogIn size={22} color="#000" />
+                        </Link>
+                    )}
+                </div>
+            </div>
+        </header>
     )
 }
