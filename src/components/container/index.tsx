05e306e5fa4efa756fc75412f0681d3387cc63c6
otaciliow <otacilio.dml@hotmail.com>
2025-02-13 16:40:10 -0300

    feat: add footer

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -2,8 +2,13 @@ import { ReactNode } from "react";
 
 export function Container({children}: {children: ReactNode}) {
     return (
-        <main className="w-full max-w-7xl mx-auto px-4 mt-4">
-            {children}
-        </main>
+        <>
+            <main className="w-full max-w-7xl mx-auto px-4 mt-4">
+                {children}
+            </main>
+            <footer className="flex items-center justify-center w-full my-8">
+                <p>&copy;&nbsp;1996 - 2025 WebCarros - Todos os direitos reservados</p>
+            </footer>
+        </>
     )
 }
