c045aca3e80bccb1050133acd80bb8f4b5d2d84e
otaciliow <otacilio.dml@hotmail.com>
2025-02-13 14:26:11 -0300

    feat: add dashboard header's component

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,11 @@
+import { Container } from "../../components/container";
+import { DashboardHeader } from "../../components/panelheader";
+
 export function Dashboard() {
     return (
-        <div>
+        <Container>
+            <DashboardHeader />
             <h1>Página Dashboard</h1>
-        </div>
+        </Container>
     )
 }
